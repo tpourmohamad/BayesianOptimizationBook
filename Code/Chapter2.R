27dda2a159ca8e36436c577f501a8753af5cc435
tpourmohamad <48219863+tpourmohamad@users.noreply.github.com>
2021-05-19 21:44:43 -0700

    Update Chapter2.R

--- Code/Chapter2.R
+++ Code/Chapter2.R
@@ -1,1 +1,14 @@
+####################################################
+### Appendix A.1 -- Getting Started
 
+library(GA)
+library(tgp)
+library(laGP)
+library(mvtnorm)
+library(ggplot2)
+library(gridExtra)
+library(CompModels)
+library(scatterplot3d)
+
+####################################################
+### Chapter 2 Code
